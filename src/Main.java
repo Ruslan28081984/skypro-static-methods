3432c47e32276ed6d89f1697f52cea1758447312
Ruslan28081984 <89201828020@mail.ru>
2022-08-03 00:04:54 +0300

    Коммит - первая задача - высокосный год

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,17 @@
 public class Main {
-    public static void  printSeparator(){
-        System.out.println("============");
-        System.out.println("++++++++++++++");
-    }
-
-    public static void printIssues(int issuesCount) {
-        System.out.println(issuesCount);
-    }
-
-    public static int sum(int[] numbers) {
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum = sum + i;
+    public static void currentYear (){
+        int year  = 2020;
+        boolean thisYear = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+        if (thisYear) {
+            System.out.println(year + " год является высокосным ");
+        } else {
+            System.out.println(year + " год не является высокосным");
         }
-        return sum;
     }
-
-    public static int valueSr(int[] valueNumbers) {
-        int value = 0;
-        int valueSr = 0;
-        for (int i = 0; i < valueNumbers.length; i++) {
-            value = value + i;
-            valueSr = value / valueNumbers.length;
-        }
-        return valueSr;
-    }
-
     public static void main(String[] args) {
-        int[] issuesByMonths = {4, 9, 10, 3, 8, 7, 1, 10, 6, 11, 4, 9};
-        printSeparator();
-        for (int i = 0; i < issuesByMonths.length; i++) {
-            printIssues(issuesByMonths[i]);
-            if ((i + 1) % 3 == 0) {
-                printSeparator();
-            }
-        }
-       int total = sum(issuesByMonths);
-        printIssues(total);
-        int total2 = valueSr(issuesByMonths);
-        printIssues(total2);
+        System.out.print("Сейчас проверим является ли год высокосным: ");
+      currentYear();
+
+
     }
 }
