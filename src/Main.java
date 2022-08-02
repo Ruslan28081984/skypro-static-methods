23116debb24de68878a5d8516bd5fdcb81d13a56
Ruslan28081984 <89201828020@mail.ru>
2022-08-03 00:24:35 +0300

    Коммит - первая задача - высокосный год

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void currentYear (){
-        int year  = 2020;
-        boolean thisYear = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+        Scanner in = new Scanner(System.in);
+        System.out.println(" Введи год, который хочешь проверить ");
+        int num = in.nextInt();
+        System.out.print("Ты ввел "+ num + " а теперь смотри результат: ");
+        boolean thisYear = (num % 4 == 0 && num % 100 != 0 || num % 400 == 0);
         if (thisYear) {
-            System.out.println(year + " год является высокосным ");
+            System.out.println(num + " год является высокосным ");
         } else {
-            System.out.println(year + " год не является высокосным");
+            System.out.println(num + " год не является высокосным");
         }
     }
+
+    public static int inConsole (){
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        return  num;
+    }
     public static void main(String[] args) {
-        System.out.print("Сейчас проверим является ли год высокосным: ");
+        System.out.println ("Сейчас проверим является ли год высокосным.  ");
       currentYear();
 
 
