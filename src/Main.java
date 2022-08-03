83d08923ea64dea89ff646c3fb5f9efd31df65cf
user1 <89201828020@mail.ru>
2022-08-03 19:05:47 +0300

    первая задача

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,25 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void currentYear (){
-        Scanner in = new Scanner(System.in);
-        System.out.println(" Введи год, который хочешь проверить ");
-        int num = in.nextInt();
-        System.out.print("Ты ввел "+ num + " а теперь смотри результат: ");
-        boolean thisYear = (num % 4 == 0 && num % 100 != 0 || num % 400 == 0);
-        if (thisYear) {
-            System.out.println(num + " год является высокосным ");
-        } else {
-            System.out.println(num + " год не является высокосным");
-        }
-    }
 
-    public static int inConsole (){
+    public static void main(String[] args) {
+        int year  = inNumberYear();
+        checkYearUp(year);
+    }
+    public static int inNumberYear () {
+        System.out.println ("Введи с клавиатуры год, который нуждается в проверке: ");
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
         return  num;
     }
-    public static void main(String[] args) {
-        System.out.println ("Сейчас проверим является ли год высокосным.  ");
-      currentYear();
-
-
+    public static void checkYearUp(int tr) {
+        System.out.println("Ты ввел "+ tr);
+        boolean thisYear = (tr % 4 == 0 && tr % 100 != 0 || tr % 400 == 0);
+        if (thisYear) {
+            System.out.println(tr + " год является высокосным ");
+        } else {
+            System.out.println(tr + " год не является высокосным");
+        }
     }
+    public static void
 }
